64119adf8a9aa767d425ce6f18b78854909435f6
Up <upcraftedlp@gmail.com>
2023-08-17 03:46:13 +0200

    make header logo clickable

--- src/components/other/LandingPage.svelte
+++ src/components/other/LandingPage.svelte
@@ -4,7 +4,7 @@
 </script>
 <nav class="w-full gap-3 py-3 min-h-fit">
     <div class="h-12 px-4 min-h-fit">
-        <div class="inline-flex items-center gap-2 pr-3"><Icon src="/assets/logo/96x.png" class="bg-c2" /><h1 class="text-3xl text-white">Vulpark</h1></div>
+        <a href="/" class="inline-flex items-center gap-2 pr-3"><Icon src="/assets/logo/96x.png" class="bg-c2" /><h1 class="text-3xl text-white">Vulpark</h1></a>
     </div>
     <hr class="mt-4 border-t-0 h-0.5 opacity-100 dark:opacity-50 bg-c2">
 </nav>
